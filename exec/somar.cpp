ff6986433fe7b8acc33837e038af9b54ea5a5d68
Ed Santilli <santilli@physics.unc.edu>
2014-10-21 13:13:29 -0400

    Added legal preamble.

--- exec/somar.cpp
+++ exec/somar.cpp
@@ -1,9 +1,32 @@
 /*******************************************************************************
- *  FILE: amrins.cpp
- *  DESC: The driver of the Navier-Stokes solver.
- *  DATE: Wed 17 Aug 2011 08:05:20 PM EDT
- *  MAIL: santilli@physics.unc.edu
+ *  SOMAR - Stratified Ocean Model with Adaptive Refinement
+ *  Developed by Ed Santilli & Alberto Scotti
+ *  Copyright (C) 2014 University of North Carolina at Chapel Hill
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ *
+ *  For up-to-date contact information, please visit the repository homepage,
+ *  https://github.com/somarhub.
  ******************************************************************************/
+
+
+// This is the driver of the Navier-Stokes solver. It contains the main() C++
+// function. This file was not written to be user-friendly and should not need
+// to be altered. -ES
+
 
 // Standard headers
 #include <iostream>
