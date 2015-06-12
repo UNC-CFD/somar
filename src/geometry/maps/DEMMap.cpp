afbb92d0038aef83a2dde3a504d3f7b2f96631b3
Alberto Scotti <ululi1970@gmail.com>
2015-06-12 16:49:27 -0400

    DEM works 2D, still iffy in 3D. Mostly cleaned up the .cpp file and prepared 3D

--- src/geometry/maps/DEMMap.cpp
+++ src/geometry/maps/DEMMap.cpp
@@ -50,8 +50,6 @@
 
 
 bool DEMMap::s_fileIsRead = false;
-RefCountedPtr<FArrayBox>  DEMMap::s_depthPtr_level0;
-RefCountedPtr<FArrayBox>  DEMMap::s_depthPtr_level1;
 Vector<RefCountedPtr<FArrayBox> >  DEMMap::s_depthPtr; 
 
 // -----------------------------------------------------------------------------
@@ -64,7 +62,7 @@ DEMMap::DEMMap ()
       const ProblemContext* ctx = ProblemContext::getInstance();
       // create vector to contain the DEMs at each level of refinement
              s_depthPtr.resize(ctx->max_level+1);
-      
+	     // fills the maps.       
         this->createBaseMap();
 
 
@@ -118,36 +116,270 @@ void DEMMap::fill_bathymetry (FArrayBox&       a_dest,
     CH_assert(destBox == horizontalDataBox(destBox));
     CH_assert(destBoxType[SpaceDim-1] == 1);
 
-    // TODO
-    // The problem is how to copy the depths. destBox is a box 
-    // which is generally contained in the domain. 
-    //    pout() << "fill_bathymetry invoked" << counter << endl;
-    //counter += 1;
+    // we determine what level has called fill_bathymetry
+    // and copy the bathymetry from the FArrayBox at the
+    // appropriate level.
+    const ProblemContext* ctx = ProblemContext::getInstance();
+    bool depthIsSet = false;
+    int level=0;
+    int levelMax=ctx->max_level;
+    RealVect DX=a_dXi;
+    for(level; level<levelMax+1; ++level)
+      {
+	  
+	if (m_lev0DXi == DX) {// AS: I am a bit uncomfortable here, as we are 
+	  // comparing Real values. Rounding errors can be a PITA.
+	  // If there is a problem, will be caught by the MayDay call        
+      a_dest.copy(*(s_depthPtr[level]), 0,a_destComp);
+      depthIsSet=true;
+      break;
 
-    if (m_lev0DXi == a_dXi) {
-     
-      
-	
-      a_dest.copy(*(s_depthPtr[0]), 0,a_destComp);
-      
-    } else {
-      //        MayDay::Error("Bad a_dXi");
-      a_dest.copy(*(s_depthPtr[1]),0,a_destComp);
-    }
+	} 
+	DX*=(ctx->refRatios[level]); 
+      }
+	if(depthIsSet==false) {MayDay::Error("DEMMap::fill_bathymetry can't determine level");}
+
 }
 
-
 // -----------------------------------------------------------------------------
-// Reads the bathymetry from the Digital Elevation Model and interpolates onto level 0 grids.
-// This also trims the region and smooths the edges.
+// Reads the bathymetry from the Digital Elevation Model and interpolates onto level grids depending on the dimensionality of the problem
 // -----------------------------------------------------------------------------
 void DEMMap::createBaseMap ()
-{
+{    const ProblemContext* ctx = ProblemContext::getInstance();
+
+  Vector<Real> depthVect,xVect,yVect;
+
+  Read_DEM_File(depthVect,xVect,yVect);
+  if((yVect.size()==1)&(SpaceDim == 3))
+    {MayDay::Error("DEMMap:: createBaseMap was given a 2D DEM file but problem is 3D");} 
   
-    std::string infile;
-    int Nx;
-    int Ny;
-    const ProblemContext* ctx = ProblemContext::getInstance();
+    if(yVect.size()==1)
+      {Create_Level_DEM_2D(depthVect,xVect);}
+    else
+      {Create_Level_DEM_3D(depthVect,xVect,yVect);}
+    return;
+}    
+//---------------------------------------------------------------------------
+// fills the 
+void DEMMap::Create_Level_DEM_3D(Vector<Real>& depthVect,
+                                 Vector<Real>& xVect,
+				 Vector<Real>& yVect)
+{   
+  // get problem context
+  const ProblemContext* ctx = ProblemContext::getInstance();
+  int Nx=xVect.size();
+  int Ny=yVect.size();
+  // create a box for the data just read.
+  Box domBox(IntVect::Zero, IntVect(D_DECL(Nx,Ny,0)), IntVect::Unit); //node centered
+  domBox.shiftHalf(SpaceDim-1, 1); // cell centered in vertical
+  domBox.enclosedCells(); // pixie dust
+  CH_assert(!domBox.isEmpty());
+  CH_assert(domBox.type() == IntVect::Zero);
+  
+  
+  // package the input vectors into FABs
+  
+  FArrayBox depthFAB(domBox, 1);
+  FArrayBox dfdxFAB(domBox,1);
+  FArrayBox dfdyFAB(domBox,1);
+
+  // cell centered in x,y and flat in z. 
+    
+    
+
+  
+  for (BoxIterator bit(domBox); bit.ok(); ++bit) {
+    const IntVect& nc = bit(); //AS: Why the ampersand? Why const? 
+    int idx = nc[1] + nc[0]*Ny;
+    depthFAB(nc) = depthVect[idx];
+  }
+  {// X derivative
+    Vector<Real> depthSlice(Nx);
+    Vector<Real> dfdx(Nx);
+    IntVect nc = IntVect::Zero;
+    for (int j = 0; j < Ny; ++j) {
+      nc[1] = j;
+      
+      // Copy slice to local vector
+      for (int i = 0; i < Nx; ++i) {
+	nc[0] = i;
+	depthSlice[i] = depthFAB(nc);
+      }
+      
+      // // Compute first derivatives
+      
+      // Just use a finite difference
+      const IntVect& e = BASISV(0);
+      nc[0] = 0;
+      dfdx[0] = (depthFAB(nc+e) - depthFAB(nc)) / (xVect[1] - xVect[0]);
+      for (int i = 1; i < Nx-1; ++i) {
+	nc[0] = i;
+	dfdx[i] = (depthFAB(nc+e) - depthFAB(nc-e)) / (xVect[i+1] - xVect[i-1]);
+      }
+      nc[0] = Nx-1;
+      dfdx[Nx-1] = (depthFAB(nc) - depthFAB(nc-e)) / (xVect[Nx-1] - xVect[Nx-2]);
+      
+      // Copy the data to a permanent holder
+      for (int i = 0; i < Nx; ++i) {
+	nc[0] = i;
+	dfdxFAB(nc) = dfdx[i];
+      }
+    }
+  }
+  pout() << "calculated dfdx " << endl;
+  {  // now the y derivative
+    Vector<Real> depthSlice(Ny);
+    Vector<Real> dfdy(Ny);
+    IntVect nc = IntVect::Zero;
+    
+    for (int i = 0; i < Nx; ++i) {
+      nc[0] = i;
+      
+      // Copy slice to local vector
+      for (int j = 0; j < Ny; ++j) {
+	nc[1] = j;
+	depthSlice[j] = depthFAB(nc);
+      }
+      
+      
+      // Just use a finite difference
+      const IntVect& e = BASISV(1);
+      nc[1] = 0;
+      dfdy[0] = (depthFAB(nc+e) - depthFAB(nc)) / (yVect[1] - yVect[0]);
+      for (int j = 1; j < Ny-1; ++j) {
+	nc[1] = j;
+	dfdy[j] = (depthFAB(nc+e) - depthFAB(nc-e)) / (yVect[j+1] - yVect[j-1]);
+      }
+      nc[1] = Ny-1;
+      dfdy[Ny-1] = (depthFAB(nc) - depthFAB(nc-e)) / (yVect[Ny-1] - yVect[Ny-2]);
+      
+      // Copy the data to a permanent holder
+      for (int j = 0; j < Ny; ++j) {
+	nc[1] = j;
+	dfdyFAB(nc) = dfdy[j];
+      }
+    }
+    
+  }    
+  pout() << "calculated dfdy " << endl;
+
+
+
+  // Stuff we need on the level=0 grid. We iterate from here on
+int nx_offset = ctx->nx_offset[0];
+Real xmin = (Real(nx_offset))*m_lev0DXi[0];
+int ny_offset = ctx->nx_offset[1];
+Real ymin = (Real(ny_offset))*m_lev0DXi[1];
+int nx = ctx->nx[0];
+int ny = ctx->nx[1];
+Real dX=m_lev0DXi[0];
+Real dY=m_lev0DXi[1];
+Box interpBox = ctx->domain.domainBox();
+interpBox.surroundingNodes();
+interpBox = flattenBox(interpBox, SpaceDim-1);
+ FArrayBox xInterp(interpBox,1);
+ FArrayBox yInterp(interpBox,1);
+ pout()<< "xmin and ymin " << xmin << " " << ymin<<endl;
+for (int level=0; level<ctx->max_level+1; ++level){
+   s_depthPtr[level] = RefCountedPtr<FArrayBox> (new FArrayBox);
+   s_depthPtr[level]->define(interpBox,1); 
+  BoxIterator bit(interpBox);
+  for (bit.reset(); bit.ok(); ++bit) {
+    const IntVect& nc = bit();
+    xInterp(nc) =  nc[0]*dX;
+    yInterp(nc) =  nc[1]*dY;
+  
+  }
+
+  // Interpolate onto our interpBox and store the results in the appropriate container
+  HermiteInterp2D(*(s_depthPtr[level]),
+     		  xInterp,
+     		  yInterp,
+     		  interpBox,
+     		  0, // xdir
+     		  1, // ydir
+     		  xVect,
+     		  yVect,
+     		  depthFAB,
+     		  dfdxFAB,
+     		  dfdyFAB);
+ // move on to the next grid
+  nx *= ctx->refRatios[level][0];
+  ny *= ctx->refRatios[level][1];
+  nx_offset *= ctx->refRatios[level][0]; 
+  ny_offset *= ctx->refRatios[level][1]; 
+  dX /= Real(ctx->refRatios[level][0]);
+  dY /= Real(ctx->refRatios[level][1]);
+  interpBox.refine(ctx->refRatios[level]);
+  xInterp.resize(interpBox,1);
+  yInterp.resize(interpBox,1);
+
+ }
+
+
+return;
+}
+
+   
+
+ 
+
+
+   
+
+void DEMMap::Create_Level_DEM_2D(Vector<Real>& depthVect, 
+				 Vector<Real>&xVect)
+
+{  const ProblemContext* ctx = ProblemContext::getInstance();
+  // prepare depthVect for interpolation
+  CubicSpline cs;
+  cs.solve(depthVect,xVect);
+  // Stuff we need on the level=0 grid
+  int nx_offset = ctx->nx_offset[0];
+  Real xmin = (Real(nx_offset))*m_lev0DXi[0];
+  int nx = ctx->nx[0];
+  Real dX=m_lev0DXi[0];
+  Box interpBox = ctx->domain.domainBox();
+  interpBox.surroundingNodes();
+  interpBox = flattenBox(interpBox, SpaceDim-1);
+  Vector<Real> xInterp(nx+1);
+  Vector<Real> depthInterp(nx+1);
+
+  for (int level=0; level<ctx->max_level+1; ++level) 
+    {// we create the coordinates where we will interpolate
+      for (int i=0; i<nx+1; ++i) {
+	xInterp[i] = xmin + dX* Real(i) ;
+      }       
+      
+      cs.interp(depthInterp,xInterp);
+      
+      // now we chomboize the result
+      s_depthPtr[level] = RefCountedPtr<FArrayBox> (new FArrayBox);
+      s_depthPtr[level]->define(interpBox,1); 
+      pout() << "interpBox level "<< level << " = " << interpBox << endl;
+      BoxIterator bit(interpBox);
+      for (bit.begin(); bit.ok(); ++bit){
+	
+	(*(s_depthPtr[level]))(bit())=depthInterp[(bit()[0]-nx_offset)]; // and now we fill it with the (hopefully) right data
+      }
+      // now we update the stuff we need for the next level
+      nx *= ctx->refRatios[level][0]; 
+      nx_offset *= ctx->refRatios[level][0]; 
+      dX /= Real(ctx->refRatios[level][0]);
+      interpBox.refine(ctx->refRatios[level]);
+      xInterp.resize(nx+1);
+      depthInterp.resize(nx+1);
+    }
+  return; }
+
+ void DEMMap::Read_DEM_File(Vector<Real>& depthVect,
+			     Vector<Real>& xVect,
+			     Vector<Real>& yVect)
+ {
+   std::string infile;
+   int Nx;
+   int Ny;
+   const ProblemContext* ctx = ProblemContext::getInstance();
 
     infile = ctx->DemFile;
     // Open the input HDF5 file.
@@ -174,7 +406,8 @@ void DEMMap::createBaseMap ()
       H5Dclose(xID);
     }
     // Read X data
-    Vector<double> xVect(Nx, quietNAN);
+    xVect.resize(Nx);
+    xVect.assign(quietNAN);
     {
         hid_t xID = H5Dopen(fileID, "/X");
         if (xID < 0) {
@@ -192,7 +425,7 @@ void DEMMap::createBaseMap ()
         H5Dclose(xID);
     }
     // Determine size of Y data
-    Vector<double> yVect;
+
     hid_t xID = H5Dopen(fileID, "/Y");
       if (xID < 0) {
 	pout() << "/Y not present in DEM file " << endl;
@@ -207,11 +440,12 @@ void DEMMap::createBaseMap ()
   
 	Ny=1;
 	yVect.resize(Ny);
+	  yVect.assign(quietNAN);
       }
       else
 	{
 	  hid_t xDSPC=H5Dget_space(xID);
-	  Ny=int( H5Sget_simple_extent_npoints( xID ));				 
+	  Ny=int( H5Sget_simple_extent_npoints( xDSPC ));				 
 	  H5Sclose(xDSPC);
 	  H5Dclose(xID);
 	
@@ -219,6 +453,7 @@ void DEMMap::createBaseMap ()
     // Read Y data
     
        yVect.resize(Ny);
+       yVect.assign(quietNAN);
       if (Ny>1) {    
             
         hid_t yID = H5Dopen(fileID, "/Y");
@@ -235,7 +470,8 @@ void DEMMap::createBaseMap ()
 	}
     
     // Read Depth data
-    Vector<double> depthVect(Nx*Ny, quietNAN);
+      depthVect.resize(Nx*Ny);
+      depthVect.assign(quietNAN);
     {
         hid_t depthID = H5Dopen(fileID, "/Depth");
         if (depthID < 0) {
@@ -258,255 +494,5 @@ void DEMMap::createBaseMap ()
         H5Fclose(fileID);
     }
 
-    // Create the level 0 grids
-    
-    ProblemDomain domain;
-    DisjointBoxLayout grids;
-    DataIterator dit;
-    {
-        // Create the domain
-      Box domBox(IntVect::Zero, IntVect(D_DECL(Nx-1,Ny-1,0)), IntVect::Unit); //node centered
-        domBox.shiftHalf(SpaceDim-1, 1); // cell centered in vertical
-        domBox.enclosedCells(); // pixie dust
-        CH_assert(!domBox.isEmpty());
-        CH_assert(domBox.type() == IntVect::Zero);
-        domain.define(domBox);
-
-        Vector<Box> vbox(1, domBox); // # of boxes and initialization list
-        Vector<int> vproc(1, 0); 
-        grids.define(vbox, vproc, domain);
-
-        dit = grids.dataIterator();
-    }
-
-    if(Ny==1)
-      { // prepare depthVect for interpolation
-	CubicSpline cs;
-	cs.solve(depthVect,xVect);
-	// Stuff we need on the level=0 gridl
-	int nx_offset = ctx->nx_offset[0];
-	Real xmin = (Real(nx_offset))*m_lev0DXi[0];
-	int nx = ctx->nx[0];
-	Real dX=m_lev0DXi[0];
-	Box interpBox = ctx->domain.domainBox();
-	interpBox.surroundingNodes();
-	interpBox = flattenBox(interpBox, SpaceDim-1);
-	Vector<Real> xInterp(nx+1);
-	Vector<Real> depthInterp(nx+1);
-
-	for (int level=0; level<ctx->max_level+1; ++level) 
-	  {// we create the coordinates where we will interpolate
-	    for (int i=0; i<nx+1; ++i) {
-	      xInterp[i] = xmin + dX* Real(i) ;
-	    }	    
-	
-	    cs.interp(depthInterp,xInterp);
-	
-	    // now we chomboize the result
-	    s_depthPtr[level] = RefCountedPtr<FArrayBox> (new FArrayBox);
-	    s_depthPtr[level]->define(interpBox,1); // Still unallocated the space
-	    pout() << "interpBox level "<< level << " = " << interpBox << endl;
-	    BoxIterator bit(interpBox);
-	    for (bit.begin(); bit.ok(); ++bit){
-	    
-	      (*(s_depthPtr[level]))(bit())=depthInterp[(bit()[0]-nx_offset)]; // and now we fill it with the (hopefully) right data
-	    }
-	    // now we update the stuff we need for the next level
-	    nx *= ctx->refRatios[level][0]; 
-	    nx_offset *= ctx->refRatios[level][0]; 
-	    dX /= Real(ctx->refRatios[level][0]);
-	    interpBox.refine(ctx->refRatios[level]);
-	    xInterp.resize(nx+1);
-	    depthInterp.reize(nx+1);
-      }
-	
-     
-	    return;}
-    
-
-    // Package the data into a LevelData<FArrayBox>.
-    LevelData<FArrayBox> depthData(grids, 1, IntVect::Zero, FlatNodeDataFactory(BASISV(SpaceDim-1))); 
-    // cell centered in x,y and flat in z. 
-    for (dit.reset(); dit.ok(); ++dit) { //only rank 0 does the work...
-        FArrayBox& depthFAB = depthData[dit];
-        BoxIterator bit(depthFAB.box());
-        int idx;
-
-        for (bit.reset(); bit.ok(); ++bit) {
-            const IntVect& nc = bit();
-            idx = nc[1] + nc[0]*Ny;
-            depthFAB(nc) = depthVect[idx];
-        }
-    }
-
-
-
-    // We need the first derivatives at each node. The values come from cubic
-    // spline interpolations of the data along all x and y coordinate lines.
-
-    // First the x derivatives...
-    LevelData<FArrayBox> dfdx(grids, 1, depthData.ghostVect(), FlatNodeDataFactory(BASISV(SpaceDim-1)));
-    for (dit.reset(); dit.ok(); ++dit) {
-        FArrayBox& dfdxFAB = dfdx[dit];
-        const FArrayBox& depthFAB = depthData[dit];
-
-        Vector<Real> depthSlice(Nx);
-        Vector<Real> dfdx(Nx);
-        IntVect nc = IntVect::Zero;
-
-        for (int j = 0; j < Ny; ++j) {
-            nc[1] = j;
-
-            // Copy slice to local vector
-            for (int i = 0; i < Nx; ++i) {
-                nc[0] = i;
-                depthSlice[i] = depthFAB(nc);
-            }
-
-            // // Compute first derivatives
-            // CubicSpline cs;
-            // cs.solve(depthSlice, xData);
-            // cs.interpFirstDeriv(dfdx, xData);
-
-            // Just use a finite difference
-            const IntVect& e = BASISV(0);
-            nc[0] = 0;
-            dfdx[0] = (depthFAB(nc+e) - depthFAB(nc)) / (xVect[1] - xVect[0]);
-            for (int i = 1; i < Nx-1; ++i) {
-                nc[0] = i;
-                dfdx[i] = (depthFAB(nc+e) - depthFAB(nc-e)) / (xVect[i+1] - xVect[i-1]);
-            }
-            nc[0] = Nx-1;
-            dfdx[Nx-1] = (depthFAB(nc) - depthFAB(nc-e)) / (xVect[Nx-1] - xVect[Nx-2]);
-
-            // Copy the data to a permanent holder
-            for (int i = 0; i < Nx; ++i) {
-                nc[0] = i;
-                dfdxFAB(nc) = dfdx[i];
-            }
-        }
-    }
-
-    // Then the y derivatives...
-    LevelData<FArrayBox> dfdy(grids, 1, depthData.ghostVect(), FlatNodeDataFactory(BASISV(SpaceDim-1)));
-    for (dit.reset(); dit.ok(); ++dit) {
-        FArrayBox& dfdyFAB = dfdy[dit];
-        const FArrayBox& depthFAB = depthData[dit];
-
-        Vector<Real> depthSlice(Ny);
-        Vector<Real> dfdy(Ny);
-        IntVect nc = IntVect::Zero;
-
-        for (int i = 0; i < Nx; ++i) {
-            nc[0] = i;
-
-            // Copy slice to local vector
-            for (int j = 0; j < Ny; ++j) {
-                nc[1] = j;
-                depthSlice[j] = depthFAB(nc);
-            }
-
-            // // Compute first derivatives
-            // CubicSpline cs;
-            // cs.solve(depthSlice, yData);
-            // cs.interpFirstDeriv(dfdy, yData);
-
-            // Just use a finite difference
-            const IntVect& e = BASISV(1);
-            nc[1] = 0;
-            dfdy[0] = (depthFAB(nc+e) - depthFAB(nc)) / (yVect[1] - yVect[0]);
-            for (int j = 1; j < Ny-1; ++j) {
-                nc[1] = j;
-                dfdy[j] = (depthFAB(nc+e) - depthFAB(nc-e)) / (yVect[j+1] - yVect[j-1]);
-            }
-            nc[1] = Ny-1;
-            dfdy[Ny-1] = (depthFAB(nc) - depthFAB(nc-e)) / (yVect[Ny-1] - yVect[Ny-2]);
-
-            // Copy the data to a permanent holder
-            for (int j = 0; j < Ny; ++j) {
-                nc[1] = j;
-                dfdyFAB(nc) = dfdy[j];
-            }
-        }
-    }
-    
-    // Where do we *want* data?
-    Box interpBox = ctx->domain.domainBox();
-    interpBox.surroundingNodes();
-    interpBox = flattenBox(interpBox, SpaceDim-1);
-    // Box interpBox(IntVect::Zero, IntVect(D_DECL(256,256,0)), IntVect::Unit);
-    pout() << "interpBox = " << interpBox << endl;
-    int nx = ctx->nx[0];
-    int nx_offset = ctx->nx_offset[0];
-    
-    int ny = ctx->nx[1];
-    int ny_offset = ctx->nx_offset[1];
-    
-    Real dX = ctx->domainLength[0]/Real(nx);
-    Real dY = ctx->domainLength[1]/Real(ny);
-    Real xmin = (Real(nx_offset))*dX;
-    Real xmax = xmin+ctx->domainLength[0];
-    Real ymin = (Real(ny_offset))*dY;
-    Real ymax = ymin+ctx->domainLength[1];
-    
-    FArrayBox xInterp(interpBox, 1);
-    FArrayBox yInterp(interpBox, 1);
-    FArrayBox depthInterp;
-
-    
-        BoxIterator bit(interpBox);
-        for (bit.reset(); bit.ok(); ++bit) {
-            const IntVect& nc = bit();
-
-            xInterp(nc) = xmin + (xmax - xmin) * Real(nc[0]) / Real(interpBox.size(0));
-            yInterp(nc) = ymin + (ymax - ymin) * Real(nc[1]) / Real(interpBox.size(1));
- // Interpolate onto our interpBox.
-    
-    }
-    depthInterp.define(interpBox, 1);
-    for (dit.reset(); dit.ok(); ++dit) { //only rank 0 does the work...
-
-	
-    HermiteInterp2D(depthInterp,
-                    xInterp,
-                    yInterp,
-                    interpBox,
-                    0, // xdir
-                    1, // ydir
-                    xVect,
-                    yVect,
-                    depthData[dit],
-                    dfdx[dit],
-                    dfdy[dit]);
-    }
-     writeFABname(&depthInterp, "depthInterp.hdf5");
-    
-	// s_depthPtr = RefCountedPtr<BoxLayoutData<NodeFArrayBox> >(new BoxLayoutData<NodeFArrayBox>);
-	// Vector<Box> vB(nProc(),domainBox);
-	// Vector<int> vP(nProc());
-	// for (int p=0; p<nProc(); ++p) {
-	//   vP[p]=p;}
-	// BoxLayout bL(vB,vP);
-	// s_depthPtr->define(bL,1,FlatNodeDataFactory(BASISV(SpaceDim-1))); // Every processor is good to go
-	// for (LayoutIterator dit=bL.layoutIterator(); dit.ok(); ++dit) {
-	//   for (BoxIterator bit(domainBox); bit.ok(); ++bit){
-	//     (*s_depthPtr)[dit](bit())=depthInterp(bit()[0]-nx_offset);
-	//   }
-	// }      
-    
-    // // // Extend the data onto a larger FAB.
-    // // {
-    // //     Box extendedBox = depthInterp.box();
-    // //     extendedBox.grow(128*hmask);
-
-    // //     FArrayBox tmpFAB(depthInterp.box(), 1);
-    // //     tmpFAB.copy(depthInterp);
-    // //     tmpFAB.shift(1, 50); // Changes the location of the ridge center.
-
-    // //     depthInterp.define(extendedBox, 1);
-    // //     depthInterp.setVal(clipDepth);
-    // //     depthInterp.copy(tmpFAB);
-    // //     depthInterp.shift(-extendedBox.smallEnd());
-    // // }
-
-}
+    pout() << "read DEM file of size " << Nx << "X" << Ny <<endl; 
+    return;}
